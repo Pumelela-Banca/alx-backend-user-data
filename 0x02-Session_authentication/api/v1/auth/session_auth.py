1b982f446e1e70ae2b4eb07131c184defe10a302
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-19 14:50:30 +0200

    session authentication files

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+"""
+Used in session authentication
+"""
+import re
+from typing import List, TypeVar
+from flask import request
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Implements session Auth
+    """
+    pass
