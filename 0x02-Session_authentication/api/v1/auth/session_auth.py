18811f6f05deb07a088659580a9e498805bd9551
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-19 15:00:18 +0200

    added session id saving

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,7 @@
 """
 Used in session authentication
 """
-import re
-from typing import List, TypeVar
+from uuid import uuid4
 from flask import request
 from api.v1.auth.auth import Auth
 
@@ -12,4 +11,16 @@ class SessionAuth(Auth):
     """
     Implements session Auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates session ID for user id
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
