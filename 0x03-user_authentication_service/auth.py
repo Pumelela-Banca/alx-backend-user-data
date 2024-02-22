6c1479feb6b7a234c8c671c2b3c2df4f3b23deed
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-23 00:48:10 +0200

    added none for when eror is raised

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -68,6 +68,6 @@ class Auth:
         try:
             user = self._db.find_user_by(email)
         except (InvalidRequestError, NoResultFound):
-            return
+            return None
         self._db.update_user(user.id, session_id=_generate_uuid())
         return user.session_id
