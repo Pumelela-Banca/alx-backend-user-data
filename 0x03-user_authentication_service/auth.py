0a19f5fa076a4e87ad5c7a54e9ed178f79de1f83
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-23 00:18:41 +0200

    added check for none user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,6 +42,8 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
+            if not user:
+                return False
         except (InvalidRequestError, NoResultFound):
             return False
         if bcrypt.checkpw(password.encode('utf-8'),
