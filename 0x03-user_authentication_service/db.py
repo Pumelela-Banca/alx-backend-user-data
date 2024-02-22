c499066f0ce28e43a1728afd535198c169b48184
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-22 07:56:56 +0200

    added find_user_by methos

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -39,4 +41,8 @@ class DB:
         self._session.commit()
         return user
 
-    def
+    def find_user_by(self, **kwargs) -> User:
+        """
+        finds user using kwargs
+        """
+        pass
