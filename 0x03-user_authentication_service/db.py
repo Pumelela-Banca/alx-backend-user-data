015d2cc18a12bd60f84d7ba2484bbeb35c9f1d6b
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-22 13:59:17 +0200

    added find_user_by method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -47,4 +47,14 @@ class DB:
         """
         finds user using kwargs
         """
-        pass
+        if not kwargs:
+            raise InvalidRequestError
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
