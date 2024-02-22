5d7dba1d6918b7b038428e1a8ad2b07516730ede
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-22 23:37:21 +0200

    added method  that saves user in db

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """
-authentication hashing of password
+authentication hashing of password, class
 """
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> str:
@@ -12,3 +14,22 @@ def _hash_password(password: str) -> str:
     new_p = password.encode("utf-8")
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(new_p, salt)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        registers user
+        """
+        new_user = self._db.find_user_by(email=email)
+        if new_user:
+            raise ValueError(f"<user's {email}> already exists")
+        new_pass = _hash_password(password)
+        return self._db.add_user(email, new_pass)
